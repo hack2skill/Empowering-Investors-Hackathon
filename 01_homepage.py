d51080ca0dc68a267eed0919428577d7aaf3f9e5
hindesh <hindeshnit19@gmail.com>
2023-08-24 18:31:43 +0530

    First commit

---
+++ 01_homepage.py
@@ -0,0 +1,22 @@
+# Import necessary libraries
+import streamlit as st
+from apikey import apikey
+from streamlit_player import st_player
+
+st.set_page_config(
+    page_title="Smart With Sebi",
+    page_icon=":detective:",
+    layout="wide",
+)
+
+with st.columns(3)[1]:
+    st.image("media/smart_with_sebi.jpeg", width=200)
+
+st.markdown("<h2 style='text-align: center; color: black;'>Welcome to Smart with SEBI portal</h1>", unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: left; color: black;'><hr> About this project:</h3>", unsafe_allow_html=True)
+# st.markdown("<h2 style='text-align: center; color: black;'>Welcome to Smart with SEBI portal</h1>", unsafe_allow_html=True)
+
+st.sidebar.success("Select a page above!")
+
+st_player("https://www.youtube.com/watch?v=csnD5EVL5z8")
---
+++ apikey.py
@@ -0,0 +1,8 @@
+apikey = "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" #Bobby key
+
+'''NOTE:
+This is the API key for the OpenAI API. It is a secret key and should not be shared with anyone.
+The key provided here is my personal key for demonstration purposes only on a trial version of the API.
+Please do not misuse this key.
+'''
+
